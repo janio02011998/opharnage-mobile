0f4dd6a509b2bcbef2649dad660dd6525ef44caf
Janio <cacaucodedev@gmail.com>
2021-03-03 13:49:36 -0300

    nlw 03 mobile lock

---
+++ src/routes.tsx
@@ -0,0 +1,55 @@
+import React, { FC } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+ 
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'; 
+
+import OrphanagesMap from './pages/OrphanagesMap'; 
+import OrphanageDetails from './pages/OrphanageDetails'; 
+import Header from './components/Header';
+
+export default function Routes(){
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5'} }}>
+                <Screen 
+                    name="OrphanagesMap" 
+                    component={OrphanagesMap}
+                />
+                
+                
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orfanato"/>
+                    }}
+                />
+                
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados"/>
+                    }}
+                />
+                
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione o mapa"/>
+                    }}
+                />
+
+            </Navigator>
+        </NavigationContainer>
+    );
+}
